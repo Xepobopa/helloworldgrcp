7294458ab99f12a85b3efc629e4040b4cafcd063
Дима Иванов <mac@Dimas-MacBook-Air.local>
2025-02-28 22:01:10 +0100

    update proto

--- services/genproto/helloworld_grpc.pb.go
+++ services/genproto/helloworld_grpc.pb.go
@@ -11,6 +11,7 @@ import (
 	grpc "google.golang.org/grpc"
 	codes "google.golang.org/grpc/codes"
 	status "google.golang.org/grpc/status"
+	emptypb "google.golang.org/protobuf/types/known/emptypb"
 )
 
 // This is a compile-time assertion to ensure that this generated file
@@ -19,101 +20,101 @@ import (
 const _ = grpc.SupportPackageIsVersion9
 
 const (
-	Greeter_SayHello_FullMethodName = "/helloworld.Greeter/SayHello"
+	SendBroadcast_Broadcast_FullMethodName = "/helloworld.SendBroadcast/Broadcast"
 )
 
-// GreeterClient is the client API for Greeter service.
+// SendBroadcastClient is the client API for SendBroadcast service.
 //
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
-type GreeterClient interface {
-	SayHello(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error)
+type SendBroadcastClient interface {
+	Broadcast(ctx context.Context, in *ReqMessage, opts ...grpc.CallOption) (*emptypb.Empty, error)
 }
 
-type greeterClient struct {
+type sendBroadcastClient struct {
 	cc grpc.ClientConnInterface
 }
 
-func NewGreeterClient(cc grpc.ClientConnInterface) GreeterClient {
-	return &greeterClient{cc}
+func NewSendBroadcastClient(cc grpc.ClientConnInterface) SendBroadcastClient {
+	return &sendBroadcastClient{cc}
 }
 
-func (c *greeterClient) SayHello(ctx context.Context, in *Request, opts ...grpc.CallOption) (*Reply, error) {
+func (c *sendBroadcastClient) Broadcast(ctx context.Context, in *ReqMessage, opts ...grpc.CallOption) (*emptypb.Empty, error) {
 	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
-	out := new(Reply)
-	err := c.cc.Invoke(ctx, Greeter_SayHello_FullMethodName, in, out, cOpts...)
+	out := new(emptypb.Empty)
+	err := c.cc.Invoke(ctx, SendBroadcast_Broadcast_FullMethodName, in, out, cOpts...)
 	if err != nil {
 		return nil, err
 	}
 	return out, nil
 }
 
-// GreeterServer is the server API for Greeter service.
-// All implementations must embed UnimplementedGreeterServer
+// SendBroadcastServer is the server API for SendBroadcast service.
+// All implementations must embed UnimplementedSendBroadcastServer
 // for forward compatibility.
-type GreeterServer interface {
-	SayHello(context.Context, *Request) (*Reply, error)
-	mustEmbedUnimplementedGreeterServer()
+type SendBroadcastServer interface {
+	Broadcast(context.Context, *ReqMessage) (*emptypb.Empty, error)
+	mustEmbedUnimplementedSendBroadcastServer()
 }
 
-// UnimplementedGreeterServer must be embedded to have
+// UnimplementedSendBroadcastServer must be embedded to have
 // forward compatible implementations.
 //
 // NOTE: this should be embedded by value instead of pointer to avoid a nil
 // pointer dereference when methods are called.
-type UnimplementedGreeterServer struct{}
+type UnimplementedSendBroadcastServer struct{}
 
-func (UnimplementedGreeterServer) SayHello(context.Context, *Request) (*Reply, error) {
-	return nil, status.Errorf(codes.Unimplemented, "method SayHello not implemented")
+func (UnimplementedSendBroadcastServer) Broadcast(context.Context, *ReqMessage) (*emptypb.Empty, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method Broadcast not implemented")
 }
-func (UnimplementedGreeterServer) mustEmbedUnimplementedGreeterServer() {}
-func (UnimplementedGreeterServer) testEmbeddedByValue()                 {}
+func (UnimplementedSendBroadcastServer) mustEmbedUnimplementedSendBroadcastServer() {}
+func (UnimplementedSendBroadcastServer) testEmbeddedByValue()                       {}
 
-// UnsafeGreeterServer may be embedded to opt out of forward compatibility for this service.
-// Use of this interface is not recommended, as added methods to GreeterServer will
+// UnsafeSendBroadcastServer may be embedded to opt out of forward compatibility for this service.
+// Use of this interface is not recommended, as added methods to SendBroadcastServer will
 // result in compilation errors.
-type UnsafeGreeterServer interface {
-	mustEmbedUnimplementedGreeterServer()
+type UnsafeSendBroadcastServer interface {
+	mustEmbedUnimplementedSendBroadcastServer()
 }
 
-func RegisterGreeterServer(s grpc.ServiceRegistrar, srv GreeterServer) {
-	// If the following call pancis, it indicates UnimplementedGreeterServer was
+func RegisterSendBroadcastServer(s grpc.ServiceRegistrar, srv SendBroadcastServer) {
+	// If the following call pancis, it indicates UnimplementedSendBroadcastServer was
 	// embedded by pointer and is nil.  This will cause panics if an
 	// unimplemented method is ever invoked, so we test this at initialization
 	// time to prevent it from happening at runtime later due to I/O.
 	if t, ok := srv.(interface{ testEmbeddedByValue() }); ok {
 		t.testEmbeddedByValue()
 	}
-	s.RegisterService(&Greeter_ServiceDesc, srv)
+	s.RegisterService(&SendBroadcast_ServiceDesc, srv)
 }
 
-func _Greeter_SayHello_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-	in := new(Request)
+func _SendBroadcast_Broadcast_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ReqMessage)
 	if err := dec(in); err != nil {
 		return nil, err
 	}
 	if interceptor == nil {
-		return srv.(GreeterServer).SayHello(ctx, in)
+		return srv.(SendBroadcastServer).Broadcast(ctx, in)
 	}
 	info := &grpc.UnaryServerInfo{
 		Server:     srv,
-		FullMethod: Greeter_SayHello_FullMethodName,
+		FullMethod: SendBroadcast_Broadcast_FullMethodName,
 	}
 	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
-		return srv.(GreeterServer).SayHello(ctx, req.(*Request))
+		return srv.(SendBroadcastServer).Broadcast(ctx, req.(*ReqMessage))
 	}
 	return interceptor(ctx, in, info, handler)
 }
 
-// Greeter_ServiceDesc is the grpc.ServiceDesc for Greeter service.
+// SendBroadcast_ServiceDesc is the grpc.ServiceDesc for SendBroadcast service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
-var Greeter_ServiceDesc = grpc.ServiceDesc{
-	ServiceName: "helloworld.Greeter",
-	HandlerType: (*GreeterServer)(nil),
+var SendBroadcast_ServiceDesc = grpc.ServiceDesc{
+	ServiceName: "helloworld.SendBroadcast",
+	HandlerType: (*SendBroadcastServer)(nil),
 	Methods: []grpc.MethodDesc{
 		{
-			MethodName: "SayHello",
-			Handler:    _Greeter_SayHello_Handler,
+			MethodName: "Broadcast",
+			Handler:    _SendBroadcast_Broadcast_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
